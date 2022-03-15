b1b0649a93c9c461dca900e8082e7e1055c50945
Kim Ellefsen <kim@ellefsen.me>
2022-03-15 12:38:01 +0100

    Build for prod
    Signed-off-by: Kim Ellefsen <kim@ellefsen.me>

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,8 +1,4 @@
-import MasonryWall from "@yeger/vue2-masonry-wall"
-
 Nova.booting((Vue, router, store) => {
-  Vue.use(MasonryWall)
-
   Vue.component("index-vapor-nova-upload", require("./components/IndexField"))
   Vue.component("detail-vapor-nova-upload", require("./components/DetailField"))
   Vue.component("form-vapor-nova-upload", require("./components/FormField"))
